f9512b05e3621c1103dadeaeffe03f0ac7b4a4dd
Oscar Poco <123397536+Oscarpoco@users.noreply.github.com>
2024-07-04 15:41:33 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Background from './components/background';
+
+function App() {
+  return (
+
+    <div id="header">
+      <Background/>
+    </div>
+
+
+    
+  );
+}
+
+export default App;
---
+++ src/components/background.js
@@ -0,0 +1,61 @@
+
+function Background()
+{
+    return(
+        
+        <div>
+            <nav>
+                <img src="logo.png" class="logo"></img>
+                <ul>
+                    <li><a href="#">Furniture</a></li>
+                    <li><a href="#">Showroom</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Trade</a></li>
+                    <li><a href="#">Clearence</a></li>
+                    <li><img src="vertical.jpg" className="icon"></img></li>
+                    <li><img src="facebook.jfif" className="icon"></img></li>
+                    <li><img src="pinterest.jfif" className="icon"></img></li>
+                    <li><img src="twitter.jfif" className="icon"></img></li>
+                </ul>
+            </nav>
+
+        <div id="about">
+            <div className="container">
+                <div className="row">
+                    <div className="about-col-1">
+                        <img src="cover.png" className="cover"></img>
+                    </div>
+                    <div className="about-col-2">
+                        <h1>ALISON <br></br>PLUS BED</h1>
+                        <h4>About the product</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit <br></br>
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut<br></br>
+                         veniam, quis nostrud exercitation ullamco laboris nisi ut  <br></br>
+                         consequat. Duis aute irure dolor in reprehenderit in voluptate<br></br>
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat <br></br>
+                        proident, sunt in culpa qui officia deserunt mollit anim id</p>
+                        <button>ENQUIRE NOW</button>
+                    </div>
+                </div>
+            </div>
+        <div id="boxes">
+            <div className="overall-box">
+            <img src="Screen-Bed.jpg"></img>
+            <img src="Screen-Bed.jpg" ></img>
+            <img src="Screen-Bed.jpg" ></img>
+            <img src="Screen-Bed.jpg" ></img>
+        
+                
+            </div>   
+        </div>    
+        </div>
+        </div>
+        
+
+       
+        
+    )
+    
+}
+
+export default Background
